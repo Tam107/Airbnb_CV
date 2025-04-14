547bafeb7159e337c2cbdf75927482b450508a2e
Tam107 <oicai107@gmail.com>
2025-04-14 12:09:08 +0700

    add create and check availability in BookingService

--- backend/src/main/java/org/example/backend/booking/application/BookingService.java
+++ backend/src/main/java/org/example/backend/booking/application/BookingService.java
@@ -1,13 +1,23 @@
 package org.example.backend.booking.application;
 
+import org.example.backend.booking.application.dto.BookedDateDTO;
 import org.example.backend.booking.application.dto.NewBookingDTO;
+import org.example.backend.booking.domain.Booking;
 import org.example.backend.booking.mapper.BookingMapper;
 import org.example.backend.booking.repository.BookingRepository;
 import org.example.backend.listing.application.LandlordService;
+import org.example.backend.listing.application.dto.ListingCreateBookingDTO;
 import org.example.backend.sharekernel.service.State;
 import org.example.backend.user.application.UserService;
+import org.example.backend.user.application.dto.ReadUserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class BookingService {
@@ -28,12 +38,41 @@ public class BookingService {
         this.landlordService = landlordService;
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public State<Void, String> create(NewBookingDTO newBookingDTO){
+        Booking booking = bookingMapper.newBookingToBooking(newBookingDTO);
+        Optional<ListingCreateBookingDTO> listingOpt = landlordService.getByListingPublicId(newBookingDTO.listingPublicId());
 
+        if (listingOpt.isEmpty()){
+            return State.<Void, String>builder().forError("Landlord public id is not found");
+        }
+        boolean alreadyBooked = bookingRepository.bookingExistsAtInterval(newBookingDTO.startDate(), newBookingDTO.endDate(), newBookingDTO.listingPublicId());
 
+        if (alreadyBooked){
+            return State.<Void, String>builder().forError("This listing is already booked for the selected dates");
+        }
 
-        return null;
+        ListingCreateBookingDTO listingCreateBookingDTO = listingOpt.get();
+
+        booking.setFkListing(listingOpt.get().listingPublicId());
+
+        ReadUserDTO connectedUser = userService.getAuthenticatedUserFromSecurityContext();
+        booking.setFkTenant(connectedUser.publicId());
+        booking.setNumberOfTravelers(1);
+
+        long numberOfNights = ChronoUnit.DAYS.between(newBookingDTO.startDate(), newBookingDTO.endDate());
+        booking.setTotalPrice( (int) (numberOfNights * listingCreateBookingDTO.price().value()));
+
+        bookingRepository.save(booking);
+        return State.<Void, String>builder().forSuccess();
     }
 
+
+    @Transactional(readOnly = true)
+    public List<BookedDateDTO> checkAvailability(UUID publicId){
+        return bookingRepository.findAllByFkListing(publicId)
+                .stream()
+                .map(bookingMapper::bookingToCheckAvailability).toList();
+    }
 
 }
