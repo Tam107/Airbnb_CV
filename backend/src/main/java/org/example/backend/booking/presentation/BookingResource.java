d11a3ab1fb95387f7b79c883d81c0847e4d0df42
Tam107 <oicai107@gmail.com>
2025-04-14 23:25:13 +0700

    add Booking Resource

---
+++ backend/src/main/java/org/example/backend/booking/presentation/BookingResource.java
@@ -0,0 +1,47 @@
+package org.example.backend.booking.presentation;
+
+import jakarta.validation.Valid;
+import org.example.backend.booking.application.BookingService;
+import org.example.backend.booking.application.dto.BookedDateDTO;
+import org.example.backend.booking.application.dto.NewBookingDTO;
+import org.example.backend.sharekernel.service.State;
+import org.example.backend.sharekernel.service.StatusNotification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/booking")
+public class BookingResource {
+
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingResource(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Boolean> create(@Valid @RequestBody NewBookingDTO newBookingDTO){
+        State<Void, String> createState = bookingService.create(newBookingDTO);
+        if (createState.getStatus().equals(StatusNotification.ERROR)){
+            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                    HttpStatus.BAD_REQUEST, createState.getError()
+            );
+            return ResponseEntity.of(problemDetail).build();
+        }else {
+            return ResponseEntity.ok(true);
+        }
+    }
+
+    @GetMapping("check-availability")
+    public ResponseEntity<List<BookedDateDTO>> checkAvailability(@RequestParam UUID listingPublicId){
+        return ResponseEntity.ok(bookingService.checkAvailability(listingPublicId));
+    }
+
+}
